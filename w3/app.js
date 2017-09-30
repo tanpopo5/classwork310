eb916dedb01a7e9d6bfc9732f01fc9d64a8ce68d
tanpopo5 <flowers.pips@gmail.com>
2017-09-30 02:40:05 -0400

    Fixed As3

--- w3/app.js
+++ w3/app.js
@@ -16,87 +16,91 @@ submit.onclick = function () {
   var totalans = document.getElementById("totalans");
 
   var count = 0;
-
-    
+　
+  var response = [];
+// qa1    
 for (var i=0; i<americano.length; i++) {
     if(americano[i].checked) {
-        var qz1 = americano[i].value;
-        console.log(qz1);
+        response[0] = (americano[i].value === "qz1-2");
         break;  
     }
 }
-for (var i=0; i<espresso.length; i++) {
-    if(espresso[i].checked) {
-        var qz2 = espresso[i].value;
-        console.log(qz2);
-        break;  
-    }
-}
-for (var i=0; i<pdcountry.length; i++) {
-    if(pdcountry[i].checked) {
-        var qz3 = pdcountry[i].value;
-        console.log(qz3);
-        break;  
-    }
-}
-for (var i=0; i<average.length; i++) {
-    if(average[i].checked) {
-        var qz4 = average[i].value;
-        console.log(qz4);
-        break;  
-    }
-}
-for (var i=0; i<benefit.length; i++) {
-    if(benefit[i].checked) {
-        var qz5 = benefit[i].value;
-        console.log(qz5);
-        break;  
-    }
-}
-for (var i=0; i<caffeine.length; i++) {
-    if(caffeine[i].checked) {
-        var qz6 = caffeine[i].value;
-        console.log(qz6);
-        break;  
-    }
-}
-
-if(qz1==="qz1-2") {
+if(response[0]) {
     message1.innerHTML = "You are right!";
     count++;
     
 }else{
     message1.innerHTML = "Hint: If you want to make cold brew by esspresso, what do you want to add?";
 }
-if(qz2==="qz2-1") {
+    
+// qa2
+for (var i=0; i<espresso.length; i++) {
+    if(espresso[i].checked) {
+        response[1] = (espresso[i].value === "qz2-1");
+        break;  
+    }
+}
+if(response[1]) {
     message2.innerHTML = "You are right!";
     count++;
     
 }else{
     message2.innerHTML = "Hint: Oops! Cappuccino has more creamy milk form than Latte.";
 }
-if(qz3==="qz3-1") {
+    
+// qa3    
+for (var i=0; i<pdcountry.length; i++) {
+    if(pdcountry[i].checked) {
+        response[2] = (pdcountry[i].value === "qz3-1");
+        break;  
+    }
+}
+if(response[2]) {
     message3.innerHTML = "You are right!";
     count++;
     
 }else{
     message3.innerHTML = "Hint: That's a tough question. Which country has a big land?";
 }
-if(qz4==="qz4-1") {
+    
+// qa4
+for (var i=0; i<average.length; i++) {
+    if(average[i].checked) {
+        response[3] = (average[i].value === "qz4-1");
+        break;  
+    }
+}
+if(response[3]) {
     message4.innerHTML = "You are right!";
     count++;
     
 }else{
     message4.innerHTML = "Hint: Let's try another answer!";
 }
-if(qz5==="qz5-3") {
+    
+// qa5
+for (var i=0; i<benefit.length; i++) {
+    if(benefit[i].checked) {
+        response[4] = (benefit[i].value === "qz5-3");
+        break;  
+    }
+}
+if(response[4]) {
     message5.innerHTML = "You are right!";
     count++;
     
 }else{
     message5.innerHTML = "Hint: Let's try another answer!";
 }
-if(qz6==="qz6-3") {
+    
+// qa6
+for (var i=0; i<caffeine.length; i++) {
+    if(caffeine[i].checked) {
+        response[5] = (caffeine[i].value === "qz6-3");
+        break;  
+    }
+}
+if(response[5]) {
     message6.innerHTML = "You are right!";
     count++;
     
@@ -104,6 +108,7 @@ if(qz6==="qz6-3") {
     message6.innerHTML = "Hint: Esresso seems more caffeine but it is not!";
 }
 
+// Messages 
 if(count==6) {
    totalans.innerHTML = "You are perfect! You got " + count + " /6 Score!";    
 }else if (count >= 5){

41b338ae4eb80743f9d55e9830a214dd62b0460c
tanpopo5 <flowers.pips@gmail.com>
2017-09-29 23:08:29 -0400

    Added Assingment4

---
+++ w4/app.js
@@ -0,0 +1,47 @@
+submit.onclick = function () {
+
+    var eightball = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"    
+    ];
+         
+var dx = new Array( 12,-20, 8,-16, 20, -4, 16, -8, 4,-12,0);
+var dy = new Array(-20, 8,-16, 12,-12, 16, -4, 20, -8, 4,0);
+var cnt = 0;
+var ball = document.getElementById("ball");
+var result = document.getElementsByName("result");
+var randomess = eightball[Math.floor(Math.random()*eightball.length)];
+    
+ var countup = function(){
+        cnt++;
+        ball.style.left = dx[cnt];
+        ball.style.top  = dy[cnt];
+        var cntid=setTimeout(countup, 100);
+        if(cnt>dx.length) {
+            clearTimeout(cntid);
+            ball.src = 'eightball2.png';
+            message.innerHTML = randomess;
+        }
+       
+    }
+    countup();   
+    
+};
